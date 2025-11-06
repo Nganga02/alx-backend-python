6577c6a13b120b00656a0e127810dddce073cc56
Nganga02 <nganganicholas50@gmail.com>
2025-11-07 00:02:35 +0300

    added db connection context managers

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+
+
+class DatabaseConnection:
+    def __init__(self, dbname):
+        self.dbname = dbname
+        self.conn = None
+
+    # initiating a database connection
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.dbname)
+        return self.conn
+
+    #closing a database connection
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.conn.close()
+
+
+
+with DatabaseConnection('users.db') as dbconn:
+    cursor = dbconn.cursor()
+    cursor.execute("""SELECT * FROM users;""")
+    users = cursor.fetchall()
+    print(users)
