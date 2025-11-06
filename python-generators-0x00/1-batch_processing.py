25b53a6d90d262391c730f49697053421853b3c2
Nganga02 <nganganicholas50@gmail.com>
2025-11-06 11:12:14 +0300

    Logging the queries from the users.db database

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -2,7 +2,6 @@
 
 import mysql.connector
 
-from functools import wraps
 from seed import connect_to_prodev
 
 
@@ -11,26 +10,24 @@ def stream_users_in_batches(batch_size):
     Generator that streams rows from the user_data table one by one.
     Only one loop is used as required.
     """
-
-    batch_buffer = []
+    offset = 0
     connection = connect_to_prodev()
     if not connection:
         return 
 
     try:
         cursor = connection.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM user_data")
 
-        # Single loop: yield each row one by one
-        for row in cursor:
-            batch_buffer.append(row)
-            if len(batch_buffer) == batch_size:
-                yield batch_buffer
-                batch_buffer = []
+        while True:
+            cursor.execute(f"SELECT * FROM user_data LIMIT {batch_size} OFFSET {offset}")
+
+            batch = cursor.fetchAll() 
+            if not batch:
+                break
 
 
-        if batch_buffer:
-            yield batch_buffer
+            yield batch
+            offset += batch_size
 
     except mysql.connector.Error as err:
         print(f"X Error streaming users: {err}")
@@ -66,9 +63,8 @@ def stream_users_in_batches(batch_size):
 
     try:
         cursor = connection.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM user_data")
+        cursor.execute(f"SELECT * FROM user_data FETCH FIRST")
 
-        # Single loop: yield each row one by one
         for row in cursor:
             batch_buffer.append(row)
             if len(batch_buffer) == batch_size:
