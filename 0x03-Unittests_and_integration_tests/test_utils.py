e6615d0ae2ba877af8866108a3ca4c3b181410eb
Nganga02 <nganganicholas50@gmail.com>
2025-11-13 01:59:27 +0300

    added test to github client

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
 from utils import (
-    access_nested_map, 
+    access_nested_map,
     get_json,
     memoize
 )
