bce192da8bae04bb9bc33d71bfb164de06544be8
Nganga02 <nganganicholas50@gmail.com>
2025-11-13 00:42:20 +0300

    added test to github client

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" A github org client test unit test module
+"""
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ A github org client test unit test module
+    """
+
+    
+    @parameterized.expand((
+        "google",
+        "abc",
+    ))
+    @patch("client.requests.get")
+    def test_org(self, org_name, mock_get):
+        mock_get.return_value.json.return_value = {"{}".format(org_name): "This is the map"}
+        test_client = GithubOrgClient(org_name)
+        test_client.org
+        self.assertEqual(test_client.org, {"{}".format(org_name): "This is the map"})
+
+        mock_get.assert_called_once()
--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
+
+
 import unittest
+from unittest.mock import patch
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import (
+    access_nested_map, 
+    get_json,
+    memoize
+)
+
 
 class TestAccessNestedMap(unittest.TestCase):
     """Class that inherits the TestCase class from python's unittest"""
@@ -11,7 +19,12 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}},("a", "b"), 2),
     ])
     def test_access_nested_map(self, nestedMap, path, expected):
-        """function to test access_nested_map function"""
+        """
+        Tests access_nested_map function
+        tests if the function return the desired output;
+        given the nested map and the path the function should return the expected results
+        
+        """
         self.assertEqual(access_nested_map(nested_map=nestedMap, path=path), expected)
 
     @parameterized.expand([
@@ -22,3 +35,47 @@ class TestAccessNestedMap(unittest.TestCase):
         """function used to test the function exception"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map=nestedMap, path=path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Tests the utils.get_json function"""
+    parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, payload):
+        """Using unittest's mock to mock an external request so as to simulate http
+        """
+        with patch('utils.requests.get') as mocked_get:
+            mocked_get.return_value.json() = payload
+            
+            mocked_get.assert_called_once_with(test_url)
+            response = get_json(url=test_url)
+            self.assertEqual(response, payload)
+
+
+
+class TestMemoize(unittest.TestCase):
+    """Class to test the utils.memoize decorator"""       
+    def test_memoize():
+        """Test the correct functioning of the memoize decorator"""
+
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+            
+        test_obj = TestClass() 
+            
+        with patch.object(TestClass, 'a_method', wraps=test_obj.a_method) as mocked_call:
+            # calling the property twice so as to access the property
+            test_obj.a_property
+            test_obj.a_property
+
+            """This ensures that the property is called once only"""
+            mocked_call.assert_called_once()
+
