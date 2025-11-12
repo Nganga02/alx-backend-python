86e94b789e7ebbcc518466c23d927f08b251ee1c
Nganga02 <nganganicholas50@gmail.com>
2025-11-12 10:26:30 +0300

    added test_utils file

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
-
 import unittest
 from parameterized import parameterized
-
 from utils import access_nested_map
 
-class TestAccessNextedMap(unittest.TestCase):
+class TestAccessNestedMap(unittest.TestCase):
     """Class that inherits the TestCase class from python's unittest"""
-
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}},("a",), {"b": 2}),
