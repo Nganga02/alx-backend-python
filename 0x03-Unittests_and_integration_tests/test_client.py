bb2aa4566a1e89b8e8c92da0e3abf629d73f70f7
Nganga02 <nganganicholas50@gmail.com>
2025-11-14 16:06:11 +0300

    unit test for client repos url property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -11,7 +11,14 @@ from client import GithubOrgClient
 class TestGithubOrgClient(unittest.TestCase):
     """ A github org client test unit test module
     """
+    # @classmethod
+    # def setUp(self):
+    #     self.test_client = GithubOrgClient()
 
+    # @classmethod
+    # def tearDown(self):
+    #     self.test_client.dispose()
+    
     @parameterized.expand((
         "google",
         "abc",
@@ -34,3 +41,18 @@ class TestGithubOrgClient(unittest.TestCase):
         )
 
         self.assertEqual(org_data, {"org": org_name, "data": "test"})
+
+    def test_public_repos_url(self):
+        """function to test the public repos url property
+        """
+
+        test_client = GithubOrgClient()
+        
+        with patch.object(
+            test_client,
+            '_public_repos_url',
+            new_callable=unittest.mock.PropertyMock
+        ) as mock_property:
+            mock_property.return_value = "test"
+
+            self.assertEqual(test_client._public_repos_url, "test")
