bda7ce4701ac63f441c5ad82096864660dc3466e
Nganga02 <nganganicholas50@gmail.com>
2025-11-16 14:31:24 +0300

    added the necessary views to the messaging app

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet)
 router.register(r'conversations/(?P<conversation_id>\d+)/messages', MessageViewSet, basename='conversation-messages')
 
