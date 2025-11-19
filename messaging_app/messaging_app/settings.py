a0e2f4a7e5d1f2d765243b6abf402b2f33293101
Nganga02 <nganganicholas50@gmail.com>
2025-11-19 12:27:01 +0300

    adding jwt

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -136,10 +136,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',  # For development
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication"
-        'rest_framework.authentication.SessionAuthentication',
+        "rest_framework_simplejwt.authentication.JWTAuthentication"
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
+
+AUTH_USER_MODEL = 'chats.User'
