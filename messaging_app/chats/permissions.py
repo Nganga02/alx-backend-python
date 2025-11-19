0df8f5d4c351e394debe49cf841ece674a4b2db5
Nganga02 <nganganicholas50@gmail.com>
2025-11-19 13:55:15 +0300

    adding authentication

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,14 +1,18 @@
-from rest_framework import permissions, Basepermission
+from rest_framework import permissions, BasePermission
 from models import (
     Conversation,
     Message
 )
 
 
-class IsParticipant(Basepermission):
-    def has_permission(self, request, view):
-        result = Conversation.objects.filter(participants=request.user)
-        if result:
-            return True
-        else:
-            return False 
+class IsParticipant(BasePermission):
+    """Class to determine if the logged in user
+    should access a conversation"""
+    def has_object_permission(self, request, view, obj):
+        return request.user in obj.participants_id.all()
+
+class IsSender(BasePermission):
+    """Class to determine if the logged in user 
+    should access the message"""
+    def has_object_permission(self, request, view, obj):
+        return obj.sender_id == request.user
