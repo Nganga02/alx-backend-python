05226bd3553981aab1fbda4e311f128e28e835b7
Nganga02 <nganganicholas50@gmail.com>
2025-12-03 12:29:11 +0300

    04: ORM for Unread messages

--- Django-signals_orm-0x04/messaging/managers.py
+++ Django-signals_orm-0x04/messaging/managers.py
@@ -4,5 +4,5 @@ from django.db import models
 
 class UnreadMessagesManager(models.Manager):
     """Unread messages custom manager"""
-    def unread_messages(self, user):
+    def unread_for_user(self, user):
         return self.filter(receiver = user, read = False)
