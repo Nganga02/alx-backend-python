e4cd9364773f88ac02105b9d947e647c947166ae
Nganga02 <nganganicholas50@gmail.com>
2025-12-03 11:36:34 +0300

    03: Added threaded conversations

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import logout, decorators
-
-from .models import CustomUser
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import api_view
+from django.db.models import Q
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from .models import CustomUser, Message
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from django.contrib import messages
 
 # Create your views here.
 @decorators.login_required
-@api_view(['GET', 'POST '])
 def delete_user(request, pk):
     #fetching the user to delete from the data
     user = get_object_or_404(CustomUser, pk = pk)
@@ -17,3 +17,24 @@ def delete_user(request, pk):
         logout(request)
         messages.success(request, "Your account has been successfully deleted")
         user.delete()
+
+
+@api_view
+@permission_classes([IsAuthenticatedOrReadOnly])
+def thread_view(request, pk):
+    #parent message
+    message_qs = Message.objects.filter(
+        Q(sender = request.user) |
+        Q(receiver = request.user), parent_message = None
+    ).select_related(
+        'sender',
+        'receiver',
+        'parent_message'
+    ).prefetch_related(
+        'replies'
+    )
+
+    conversation = [msg.get_thread for msg in message_qs]
+    return Response({
+        'conversation':conversation
+    })
